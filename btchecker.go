fcc5565c40f94389b2981b4d4c55c5a9e5af0bf0
Caius Durling <dev@caius.name>
2013-12-18 23:34:01 +0000

    V1 of bt checker

--- btchecker.go
+++ btchecker.go
@@ -2,11 +2,35 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
-	"log"
-	"io/ioutil"
 )
+
+type ServiceResponse struct {
+	ServiceLineType        string
+	Infinity               bool
+	SupportsVision         string
+	ExchangeState          string
+	ReadyDate              string
+	CLEAN_BOTTOM_DOWNSPEED string
+	CLEAN_BOTTOM_UPSPEED   string
+	CLEAN_TOP_DOWNSPEED    string
+	CLEAN_TOP_UPSPEED      string
+}
+
+func (service *ServiceResponse) ExchangeEnabled() bool {
+	return service.ExchangeState == "E"
+}
+
+type CheckerResponse struct {
+	Telephone        string
+	ExchangeName     string
+	ExchangeCode     string
+	ServiceLineTypes []ServiceResponse
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -15,8 +39,7 @@ func main() {
 
 	phoneNumber := os.Args[1]
 
-	// url := "http://www.productsandservices.bt.com/consumerProducts/v1/productAvailability.do?format=json&telephone=" + phoneNumber
-	url := "http://dev.hentan.caius.name/bt.json?format=json&telephone=" + phoneNumber
+	url := "http://www.productsandservices.bt.com/consumerProducts/v1/productAvailability.do?format=json&telephone=" + phoneNumber
 
 	resp, err := http.Get(url)
 	if err != nil {
@@ -28,21 +51,33 @@ func main() {
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body);
+	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// println(string(body))
+	if os.Getenv("DEBUG") != "" {
+		println(string(body))
+	}
 
-	var data map[string]interface{}
+	var data CheckerResponse
 	if err = json.Unmarshal(body, &data); err != nil {
 		log.Fatal(err)
 	}
 
-	println(string(data["exchangeName"]))
+	fmt.Printf("%s served by %s (%s)\n", data.Telephone, data.ExchangeName, data.ExchangeCode)
+	for _, service := range data.ServiceLineTypes {
+		if service.Infinity == true {
+			if service.ExchangeState == "P" {
+				fmt.Printf("Infinity service pending; ready date is %s\n", service.ReadyDate)
+			} else if service.ExchangeState == "E" {
+				fmt.Println("Infinity service already enabled!")
+			} else {
+				fmt.Println("Infinity service status unknown :-(")
+			}
+		} else {
+			fmt.Println("Infinity service status unknown :-(")
+		}
+		break
+	}
 }
-
-/*
-{"salesFilter1Date":"01-03-2013","exchangeName":"MARKET DRAYTON","salesFilter1Switch":"ON","telephone":"01630658343","serviceLineTypes":[{"supportsMulticast":"RED","CLEAN_TOP_DOWNSPEED":"40000","readyDate":"31-05-2014","exchangeState":"P","supportsVision":"RED","infinity":true,"CLEAN_BOTTOM_DOWNSPEED":"39000","CLEAN_BOTTOM_UPSPEED":"9000","capacityDate":"","CLEAN_TOP_UPSPEED":"10000","bbUpsellAvailable":"N","bb":"GREEN","serviceLineType":"BBSLT_UPTO_40M_10M_FTTCWBC"},{"supportsMulticast":"RED","readyDate":"","speed":"7M","capacityDate":"","exchangeState":"E","minRangeSpeed":"4.5M","bbUpsellAvailable":"N","maxRangeSpeed":"11.5M","bb":"GREEN","supportsVision":"GREEN","infinity":false,"serviceLineType":"BBSLT_UPTO_8M_WBC"},{"supportsMulticast":"RED","CLEAN_TOP_DOWNSPEED":"80000","readyDate":"31-05-2014","exchangeState":"P","supportsVision":"RED","infinity":true,"CLEAN_BOTTOM_DOWNSPEED":"79900","CLEAN_BOTTOM_UPSPEED":"20000","capacityDate":"","CLEAN_TOP_UPSPEED":"20000","bbUpsellAvailable":"N","bb":"GREEN","serviceLineType":"BBSLT_UPTO_80M_20M_FTTCWBC"},{"supportsMulticast":"RED","CLEAN_TOP_DOWNSPEED":"40000","readyDate":"31-05-2014","exchangeState":"P","supportsVision":"RED","infinity":true,"CLEAN_BOTTOM_DOWNSPEED":"39000","CLEAN_BOTTOM_UPSPEED":"1900","capacityDate":"","CLEAN_TOP_UPSPEED":"2000","bbUpsellAvailable":"N","bb":"GREEN","serviceLineType":"BBSLT_UPTO_40M_FTTCWBC"}],"exchangeCode":"WNMD"}
-*/
